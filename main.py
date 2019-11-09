ac86026a6020206aa010389d3a54ebff461783c8
eb88ibyw <soroosh.arasteh@fau.de>
2019-11-09 12:16:39 +0100

    Update, not working yet

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class OSMdata:
     You can specify the area by giving the longitude and the latitude of a specific point on the earth
     and then specify a radius around that point.
     '''
-    def __init__(self, lat, lon, radius):
+    def __init__(self, lon, lat, radius):
         self.lat = lat
         self.lon = lon
         self.radius = radius
@@ -25,7 +25,7 @@ class OSMdata:
         :param lat2: Longitude of the second point
         :return: Returns the distance between two points in meters
         '''
-        # convert decimal degrees to radians
+        # converts decimal degrees to radians
         lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
         # Haversine formula
@@ -33,87 +33,89 @@ class OSMdata:
         dlat = lat2 - lat1
         a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
         c = 2 * asin(sqrt(a))
-        r = 6371 # Radius of earth in kilometers. Use 3956 for miles.
+        r = 6371 #Radius of earth in kilometers. Use 3956 for miles.
         return c * r
 
-    def checkfunction(self, newlon, newlat, newradius):
+
+    def checkfunction(self):
         """
         This function first of all checks which nodes are in our desired area.
         Then retrieves the tags of these nodes. And finally, retrieves the tags
         of ways created by these nodes and relations created by all the
         nodes, ways, and relations we have so far.
         """
-        f = input('Enter the name of your OSM file, e.g. map.osm ')
-        with open(f) as fhand:
+        osm_file = 'the name of your OSM file, e.g. map.osm'
+        with open(osm_file) as fhand:
             fhand = fhand.read()
             tree = ET.fromstring(fhand)
 
-        # Tags related to nodes:
-        print('''\n********************************************************************************************************************
-    ********************************************************************************************************************
-    ********************************************************************************************************************
-                                              Nodes\nHere you will see the tags derived from the nodes. Ways and relations in the following.\n\n''')
-        first = tree.findall('node')
-        id_ha = list() # A list to store all the id's of all desired OSM elements.
-        for item in first:
-            latosm = (float(item.get('lat')))
-            lonosm = (float(item.get('lon')))
-            distant = haversine(newlon, newlat, lonosm, latosm)
-            if distant <= newradius:
-                id_ha.append(item.get('id'))
-                second = item.findall('tag')
-                for item2 in second:
-                    print('KEY:', item2.get('k'), ' | VALUE:', item2.get('v'))
-                if len(second) > 0:
-                    print('-----------------------------------------------------------------------------------------------------\n') #Separates tags of each node from the other nodes
-        # Tags related to ways:
-        print('''********************************************************************************************************************
-    ********************************************************************************************************************
-    ********************************************************************************************************************
-                                                  Ways\n''')
-        repeatid = list() # A list to find the repeated id's to avoid printing
-        repeatid.append('a')
-        first = tree.findall('way')
-        for item in first: # Looping through ways
-            wayid = item.get('id') # The way's id
-            second = item.findall('nd')
-            if all(t != wayid for t in repeatid):
-                for item2 in second: # Looping through nd's
-                    third = item2.get('ref')
-                    if any( i == third for i in id_ha): # Looping through our id list from before
-                        id_ha.append(wayid) # Add the way id to our id list
-                        repeatid.append(wayid) # So that we don't print it anymore
-                        fourth = item.findall('tag')
-                        for item3 in fourth:
-                            print('KEY:', item3.get('k'), ' | VALUE:', item3.get('v'))
-                        if len(fourth) > 0:
-                            print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n')  # Separates tags of each way from the other ways
-                        break
+            # Tags related to nodes:
+            print('''Nodes\nHere you will see the tags derived from the nodes. Ways and relations in the following.\n\n''')
+            first = tree.findall('node')
+            
+            # A list to store all the id's of all desired OSM elements.
+            id_s = list()
+            for item in first:
+                distance = self.haversine(self.lon, self.lat, float(item.get('lon')), float(item.get('lat')))
+                if distance <= self.radius:
+                    id_s.append(item.get('id'))
+                    second = item.findall('tag')
+                    for item2 in second:
+                        print('KEY:', item2.get('k'), ' | VALUE:', item2.get('v'))
 
-        # Tags related to relations:
-        print('''************************************************************************************************************************************************
-    ************************************************************************************************************************************************
-    ************************************************************************************************************************************************
-                                                      Relations\n''')
-        repeatid = list() # A list to find the repeated id's to avoid printing
-        repeatid.append('a')
-        first = tree.findall('relation')
-        for item in first:
-            relationid = item.get('id') #The relation's id
-            second = item.findall('member')
-            if all(t != relationid for t in repeatid):
-                for item2 in second: # Looping through member's
-                    third = item2.get('ref')
-                    for i in id_ha: # Looping through our id list from before
-                        if third == i:
-                            id_ha.append(relationid) # Add the relation id to our id list
-                            repeatid.append(relationid)  # So that we don't print it anymore
-                            fourth = item.findall('tag') # The problem is in here!!!
+                    # Separates tags of each node from the other nodes
+                    if len(second) > 0:
+                        print('-----------------------------------------------------------------------------------------\n')
+
+            # Tags related to ways:
+            print('''Ways\n''')
+            repeatid = list() # A list to find the repeated id's to avoid printing
+            repeatid.append('a')
+            first = tree.findall('way')
+            for item in first: # Looping through ways
+                wayid = item.get('id') # The way's id
+                second = item.findall('nd')
+                if all(t != wayid for t in repeatid):
+                    for item2 in second: # Looping through nd's
+                        third = item2.get('ref')
+                        if any( i == third for i in id_s): # Looping through our id list from before
+                            id_s.append(wayid) # Add the way id to our id list
+                            repeatid.append(wayid) # So that we don't print it anymore
+                            fourth = item.findall('tag')
                             for item3 in fourth:
                                 print('KEY:', item3.get('k'), ' | VALUE:', item3.get('v'))
+
+                            # Separates tags of each way from the other ways
                             if len(fourth) > 0:
-                                print('---------------------------------------------------------------------------------------------------------------------------\n')  # Separates tags of each relation from the other relations
+                                print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n')
                             break
+
+            # Tags related to relations:
+            print('''Relations\n''')
+
+            # A list to find the repeated id's to avoid printing
+            repeatid = list()
+            repeatid.append('a')
+            first = tree.findall('relation')
+            for item in first:
+                relationid = item.get('id') #The relation's id
+                second = item.findall('member')
+                if all(t != relationid for t in repeatid):
+                    for item2 in second: # Looping through member's
+                        third = item2.get('ref')
+                        for i in id_s: # Looping through our id list from before
+                            if third == i:
+                                id_s.append(relationid) # Add the relation id to our id list
+                                repeatid.append(relationid)  # So that we don't print it anymore
+                                fourth = item.findall('tag') # The problem is in here!!!
+                                for item3 in fourth:
+                                    print('KEY:', item3.get('k'), ' | VALUE:', item3.get('v'))
+
+                                # Separates tags of each relation from the other relations
+                                if len(fourth) > 0:
+                                    print('------------------------------------------------------------------------------\n')
+                                break
+
 
 
 if __name__ == "__main__":
@@ -124,4 +126,5 @@ if __name__ == "__main__":
     user_radius = input('Enter your desired radius in meters: ')
     user_radius = float(user_radius)
 
-    checkfunction(user_lon, user_lat, user_radius)
+    osm = OSMdata(user_lon, user_lat, user_radius)
+    osm.checkfunction()
